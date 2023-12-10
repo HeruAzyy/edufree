be001fe9ce646f72d2dbaed7eb4048ed2307538b
Heru Azy <heruazyp@gmail.com>
2023-12-11 00:19:25 +0700

    Start build: Edufree

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['*.html'],
+	theme: {
+		fontFamily: {
+			poppins: ['Poppins']
+		},
+		container: {
+			center: true
+		},
+		extend: {
+			colors: {
+				blue: '#1C1E53',
+				yellow: '#FCD980'
+			}
+		}
+	},
+	plugins: []
+};
